e8b82de1f25c26b95daac9b3fb6427869a9f8c85
Jacci <33001576+jacciz@users.noreply.github.com>
2022-02-06 16:19:40 -0600

    adding projects and stuff

--- R/app_server.R
+++ R/app_server.R
@@ -6,19 +6,19 @@
 #' @noRd
 app_server <- function( input, output, session ) {
   # Your application server logic 
-  output$about <- renderPlot({
-    plot(iris)
-  })
-  output$experience <- renderPlot({
-    plot(mtcars)
-  })
-  output$education <- renderPlot({
-    plot(airquality)
-  })
-  output$interests <- renderPlot({
-    plot(AirPassengers)
-  })
-  output$awards <- renderPlot({
-    plot(anscombe)
-  })
+  # output$about <- renderPlot({
+  #   plot(iris)
+  # })
+  # output$experience <- renderPlot({
+  #   plot(mtcars)
+  # })
+  # output$education <- renderPlot({
+  #   plot(airquality)
+  # })
+  # output$interests <- renderPlot({
+  #   plot(AirPassengers)
+  # })
+  # output$awards <- renderPlot({
+  #   plot(anscombe)
+  # })
 }
--- R/app_ui.R
+++ R/app_ui.R
@@ -16,10 +16,17 @@ app_ui <- function(request) {
         refs = c(
           "about" = "About",
           "experience" = "Experience",
-          'projects' = "Personal projects",
+          'dash_projects' = "Data Viz & Web apps",
+          'coding_projects' = "Coding projects",
           "education" = "Education",
           "interests" = "Interests",
           "awards" = "Awards"
+          # tags$span(
+          #   HTML(
+          #     '<i class="fa fa-first-aid" style = "color:#428BCA;"></i><p style="display:inline-block;">&ensp;Injury&emsp;&emsp;&nbsp;</p>'
+          #   )
+          # )
+          
         ),
         color = "orange"
       ),
@@ -27,14 +34,18 @@ app_ui <- function(request) {
         # About
         resume_section(
           id = "about",
-          h2('Jacci Ziebert'),
+          h2("Hi, I'm Jacci Ziebert"),
           h3('Madison, WI jacciziebert@gmail.com'),
-          span("I work as a data analyst in the public sector. I got my Master's in transportation planning
-                     and have worked at my state's Department of Transportation for 5 years.
-                     
-               In my positions, I like to look for ways to streamline manual tasks, make dashboards, 
-               and to translate our data to a user-friendly visual for the general public. I use R notebooks 
-               to do most of my analysis and data viz. I also have a background in GIS.")
+          span(
+            "I am a data analyst and transportation enthusiast. I started my career at the Wisconsin Department of Transportation doing travel forecasting. Learning Excel and macros, I improved our forecasting worksheets. My next position as a highway safety analyst I stepped up my programming game. I took a graduate-level programming course and sparked a passion for R.
+
+I'm always striving to increase my skill set and I hope to make maps more visually appealing while showing concise information.
+
+          Fast forward a few years
+I like to look for ways to streamline manual tasks, make dashboards, and to translate our data to a user-friendly visual for the general public. I use R notebooks to do my analysis and data viz. I also have a background in GIS.
+
+I spend my free time as a strategy board game enthusiast, a cyclist, a kitchen experimenter, a bike tourer, a coffee roaster, and a solo traveler. I'm always on the lookout for a good pour-over coffee."
+          )
         ),
         # experience
         resume_section(
@@ -45,35 +56,52 @@ app_ui <- function(request) {
         ),
         # projects
         resume_section(
-          id = "projects",
-          h4('Personal projects'),
+          id = "dash_projects",
+          h4('Data Viz & Dashboards'),
           tagList(tags$span(
             HTML(
-              "<h6>A Wisconsin Crash Statistics Dashboard built with R Shiny, leaflet, plotly under a golem framework.</h4>
-            <img src=www/crash_dashboard.png style=height:400px;display:inline-block;>")))
+              "<p>A Wisconsin Crash Statistics Dashboard built with R Shiny, leaflet, plotly under a golem framework. Switching to golem, I found that the app became extremely slow. Through performance testing, I found the bottleneck was tied to simply loading the data. I found SQLite to be an optimal solution as it is saved locally inside the package while the data is stagnant. View it <a href='https://jacciz.shinyapps.io/Shiny_Crashes/'>here</a>.</p> <img src=www/crash_dashboard.png style=height:600px;display:inline-block;>"))),
+          tagList(tags$span(
+            HTML(
+              "<p>I developed an R package to import and analyze our crash data inside an R environment. It came to a point where I was constantly sourcing the same series scripts. It was logical to make these scripts into an R package. Furthermore, the package has functions that query certain crash flags. This way I don't have to look up the query of each crash flag; it's just written inside the function. View the code <a href='https://jacciz.shinyapps.io/Shiny_Crashes/'>here</a>.</p> <img src=www/crash_dashboard.png style=height:600px;display:inline-block;>"))),
+          tagList(tags$span(
+            HTML(
+              "<p>I wrote a parameterized R Markdown to fully automate and create crash data summaries of 72 counties. In the past, each county was done by hand using Word. View Dane County's Safety Summary <a href='tiny.cc/46unuz'>here</a>.</p> View it <a href='https://jacciz.shinyapps.io/Shiny_Crashes/'>here</a>.</p> <img src=www/crash_dashboard.png style=height:600px;display:inline-block;>"))),
+          tagList(tags$span(
+            HTML(
+              "<p>An ameateur coffee roaster, I made this dashboard to visualize my roasts. The tricky part was adding the RoR (rate of return) curves onto the graph as this is calculated via an algorethm written in Python inside Artisan software. I found this piece of code and rewrote part of it so it would work for my app. Next I used the reticulate package so the app can read Python code. </p> <img src=www/crash_dashboard.png style=height:400px;display:inline-block;>")))
+        ),
+        # personal projects
+        resume_section(
+          id = "coding_projects",
+          h4('Coding Projects'),
+          tagList(tags$span(
+            HTML(
+              "<p>Working inside an R environemnt, I made an R package for data analysis for our crash databases. The crash form was changed in 2017, so this is able to import old and new data into a single dataframe.</p> <a href='https://github.com/jacciz/wisdotcrashdatabase/blob/master/wisdotcrashdatabase_1.00.pdf'>here.</a>")))
         ),
         # education
-        resume_section(
-          id = "education",
-          h4('education'),
-          tagList(tags$span(
-            HTML(
-              "<span>University of Wisconsin – Madison<br>
+        resume_section(id = "education",
+                       h4('education'),
+                       tagList(tags$span(
+                         HTML(
+                           "<span>University of Wisconsin – Madison<br>
               Master of Urban and Regional Planning<br>
-              Certificate in Transportation Management and Policy</span>"))
-        )
-        ),
+              Certificate in Transportation Management and Policy</span>"
+                         )
+                       ))), 
         # interests
         resume_section(
           id = "interests",
           h4('interests'),
-          plotOutput("interests")
-        ),
+          p(
+            "Adventure is what motivates me. I love to do all kinds of biking - road, gravel, cyclocross, traveling in my campervan."
+          )
+        ), 
         # awards
         resume_section(
           id = "awards",
           h4('awards'),
-          plotOutput("awards")
+          p("My crash dashboard won runner-up")
         )
       )
     )
